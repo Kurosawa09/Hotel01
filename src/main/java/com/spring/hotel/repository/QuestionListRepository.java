1d99e9ae82a348af400f723289b5c27db0dc805b
Kurosawa09 <hatalin09@gmail.com>
2022-01-14 13:19:07 +0900

    템플릿 수정, 로그인/후기 jsp 수정

--- src/main/java/com/spring/hotel/repository/QuestionListRepository.java
+++ src/main/java/com/spring/hotel/repository/QuestionListRepository.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring.hotel.vo.QuestionListVO;
+import com.spring.hotel.vo.ReplyVO;
+
+
+
 
 @Repository
 public class QuestionListRepository 
@@ -27,9 +31,9 @@ public class QuestionListRepository
 	         e.printStackTrace();
 	      }      
 	      return result;
-	   
 	}
 
+	
 	public int insertList(QuestionListVO newQuestion) 
 	{
 		  int result = 0;
@@ -44,6 +48,109 @@ public class QuestionListRepository
 	         e.printStackTrace();
 	      }      
 	      return result;
+	}
+
+	public QuestionListVO selectQuestion(int listNum) {
+		QuestionListVO result = null;		
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.selectQuestion(listNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
+
+	public int deleteQuestion(int listNum) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.deleteQuestion(listNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
+
+	public int updateList(QuestionListVO questionList) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			
+			result = mapper.updateList(questionList);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	public ArrayList<ReplyVO> readAllReply(int listNum) {
+		ArrayList<ReplyVO> result = null;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.readAllReply(listNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
+
+
+	public int replyWrite(ReplyVO reply) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.replyWrite(reply);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+
+	public int deleteReply(int replyNum) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.deleteReply(replyNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
+
+
+	public int deleteQuestionReply(int listNum) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.deleteQuestionReply(listNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
+	}
+
+
+	public int updateReply(ReplyVO updateReply) {
+		int result = 0;
+		QuestionListMapper mapper = null;
+		try {
+			mapper = session.getMapper(QuestionListMapper.class);
+			result = mapper.updateReply(updateReply);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return result;
 	}
 
 }
